b0cfa92d08a92d437c6cd51b43a5e6cba03ff523
Jack Finnis <finnis.j@etoncollege.org.uk>
2021-08-02 19:25:19 +0100

    Initial Commit

---
+++ Messages/MessagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagesApp.swift
+//  Messages
+//
+//  Created by William Finnis on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MessagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
